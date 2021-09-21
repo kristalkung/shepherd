6c051f5a03369540e0b861bd4447be94c9dabffc
Kristal Kung <kristalkung@gmail.com>
2021-09-21 22:22:22 +0000

    added root.html

--- server.py
+++ server.py
@@ -13,7 +13,11 @@ app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
 
+@app.route('/')
+def homepage():
+    """View homepage."""
 
+    return render_template('root.html')
 
 if __name__ == '__main__':
     connect_to_db(app)
