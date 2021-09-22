d020c5d51fd61483474341d49ec0eae30c3c50e7
Kristal Kung <kristalkung@gmail.com>
2021-09-22 23:04:48 +0000

    added applications.js to store application options and fields for each option

---
+++ static/applications.js
@@ -0,0 +1,19 @@
+export const ApplicationOptions = [
+  {
+    optionType: 'fixed',
+    companyName: 'Company Name',
+    contactEmail: 'Contact Email',
+    projectName: 'Project Name',
+    startDate: 'Project Start Date',
+    endDate: 'Expected End Date',
+    coverageRequested: 'Coverage Requested',
+  },
+  {
+    optionType: 'flexible',
+    companyName: 'Company Name',
+    contactEmail: 'Contact Email',
+    projectName: 'Project Name',
+    projectType: 'Project Type',
+    coverageRequested: 'Coverage Requested'
+  }
+]
