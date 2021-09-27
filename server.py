30f1fd7d239d73eb4b1971159d6621d9afc6c925
Kristal Kung <kristalkung@gmail.com>
2021-09-27 05:32:31 +0000

    added comments to server.py

--- server.py
+++ server.py
@@ -4,7 +4,6 @@ from flask import (Flask, render_template, request, flash, session, redirect, js
 from model import connect_to_db
 from validate_email import validate_email
 from fpdf import FPDF
-import webbrowser
 
 import crud
 import model
@@ -41,13 +40,18 @@ def application_type(option_type):
 
 @app.route('/submission', methods=['POST'])
 def validate_submission():
+    """Receives form data from applications.jsx and does the following:
+        - validates email and coverage_requested (must be int)
+        - adds form data to db
+        - creates pdf of form data and saves to local"""
 
     option_type = request.json.get('option_type')
     company_name = request.json.get('companyName')
     contact_email = request.json.get('email')
     coverage_requested = request.json.get('coverage')
     project_type = request.json.get('projectType')
-    print(f'******project_type: {project_type}')
+    
+    # from p3-validate-email package
     is_valid_email = validate_email(email_address=contact_email,
                                     check_format=True,
                                     check_blacklist=True,
@@ -66,10 +70,6 @@ def validate_submission():
     elif option_type == 'fixed':
         if coverage_requested.isdigit() == False:
             return '"invalid coverage"'
-
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.set_font("Arial", size = 20)
     
     if option_type == 'flexible':
         form_option = 'Flexible'
@@ -91,12 +91,17 @@ def validate_submission():
 
     new_app_id = new_app.application_id
 
+    # for making the pdf:
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size = 20)
     pdf.cell(200, 10, txt=f'Shepherd {form_option} Application Form', ln = 0, align = 'C')
     pdf.cell(200, 10, txt='', ln = 1, align = 'C')
     pdf.cell(200, 10, txt='', ln = 2, align = 'C')
     pdf.set_font("Arial", size = 15)
     line_no = 3
     
+    # prints out a new line for each field in form_fields
     for key in form_fields.keys():
         text = f'{key}: {form_fields[key]}'
         pdf.cell(200, 10, txt=text, ln = line_no, align = 'L')
@@ -104,15 +109,15 @@ def validate_submission():
         print(f'pdf txt is this: {text}')
     
     pdf.output(f'application{new_app_id}.pdf')
+    pdf_name = f"application{new_app_id}.pdf"
 
-    to_return = f"application{new_app_id}.pdf"
-
-    return jsonify(to_return)
+    return jsonify(pdf_name)
 
 @app.route('/cookie')
 def valid_auth_cookie():
     """Determines if valid auth cookie exists."""
 
+    # default invalid_cookie to true
     invalid_cookie = True
 
     if 'auth' in request.cookies:
@@ -121,12 +126,13 @@ def valid_auth_cookie():
 
     return jsonify(invalid_cookie)
 
-@app.route('/form/<data>', methods=['GET'])
-def show_pdf(data):
-    """Displays PDF in browser after successful submission"""
-    new = 2
-    url = f'/Users/kristalkung/vagrant/hb-dev/src/shepherd/takehome/{data}.pdf'
-    webbrowser.open(url,new=new)
+# TODO: complete show_pdf(data)
+# @app.route('/form/<data>', methods=['GET'])
+# def show_pdf(data):
+#     """Displays PDF in browser after successful submission"""
+
+#     url = f'/Users/kristalkung/vagrant/hb-dev/src/shepherd/takehome/{data}.pdf'
+#     webbrowser.open(url,new=new)
 
 
 
