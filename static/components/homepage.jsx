2c775db12f20e7069d1904ee72b24f5d182b6681
Kristal Kung <kristalkung@gmail.com>
2021-09-27 05:06:59 +0000

    changed homepage to include just two buttons

--- static/components/homepage.jsx
+++ static/components/homepage.jsx
@@ -9,13 +9,13 @@ function Homepage() {
     <div className='homepage'> 
       <h2 className='text-header'>Hi. Click on an application option below</h2>
       <div className='app-options'>
-        <div id='option-fixed'  className='text'>
-          Fixed Coverage Application: 
-          <Link className='app-link' to='/application/fixed' type='submit' value='fixed' name='option' >Fixed</Link>
+        <div id='option-fixed'  className='text option-fixed'>
+          
+          <Link className='btn btn-warning' to='/application/fixed' type='submit' value='fixed' name='option' >Fixed Coverage Application </Link>
         </div>
         <div id='option-flexible' className='text'>
-          Flexible Coverage Application: 
-          <Link className='app-link' to='/application/flexible' type='submit' value='flexible' name='option'>Flexible</Link>
+          
+          <Link className='btn btn-warning' to='/application/flexible' type='submit' value='flexible' name='option'>Flexible Coverage Application</Link>
         </div>
       </div>
     </div>
