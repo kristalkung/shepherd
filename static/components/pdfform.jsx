194563bd09de8b4db21a29f949b3bcaefe554bb0
Kristal Kung <kristalkung@gmail.com>
2021-09-27 05:33:24 +0000

    commented out pdfform for now

--- static/components/pdfform.jsx
+++ static/components/pdfform.jsx
@@ -1,18 +1,18 @@
 "use strict";
 
 
-function PdfForm() {
-    const application_form = location.pathname.slice('/form/'.length);
-    console.log(application_form)
+// function PdfForm() {
+//     const application_form = location.pathname.slice('/form/'.length);
+//     console.log(application_form)
 
-    // fetch(`/form/${application_form}`)
-    // .then(response => response.json())
-    // .then(data => console.log(data))
+//     // fetch(`/form/${application_form}`)
+//     // .then(response => response.json())
+//     // .then(data => console.log(data))
 
-    return (
-        <div>
-            pdf form
-            {/* <embed src='/${application_form}' width="800px" height="2100px" >pdf</embed> */}
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             pdf form
+//             {/* <embed src='/${application_form}' width="800px" height="2100px" >pdf</embed> */}
+//         </div>
+//     )
+// }
